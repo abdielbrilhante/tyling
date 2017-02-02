3d711a064d1303ad0a65e74a794206a79315fc87
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-02-02 11:15:39 -0300

    Add KPackage folder structure

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,1 +1,9 @@
+var Window = function (client) {
+  this.client   = client;
+  this.geometry = client.geometry;
+  this.desktop  = client.desktop;
+};
 
+var spacing = {
+  gap: 4, lr: 12, tb: 12
+};
