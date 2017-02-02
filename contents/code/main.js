8b357dc71c482976926d6159568acddabe238156
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-02-02 11:30:48 -0300

    Add WindowGroup class
    This class merely serves to hold all open windows to better select and filter KWin clients when needed. A good example (and a built in function) is filtering tileable (_NET_WM_WINDOW_TYPE_NORMAL) windows to only get those in the currently active desktop.

--- contents/code/main.js
+++ contents/code/main.js
@@ -7,3 +7,46 @@ var Window = function (client) {
 var spacing = {
   gap: 4, lr: 12, tb: 12
 };
+
+var WindowGroup = function () {
+  this.storage = {};
+
+  var allClients = workspace.clientList();
+
+  for (var i = 0; i < allClients.length; i++) {
+    this.add(allClients[i]);
+  }
+};
+
+WindowGroup.prototype.window = function (id) {
+  return this.storage[id];
+};
+
+WindowGroup.prototype.filterByDesktop = function (desktop) {
+  var keys = Object.keys(this.storage);
+  var f = [];
+
+  for (var i = 0; i < keys.length; i++) {
+    var client = this.storage[keys[i]].client;
+
+    if (client.desktop == desktop) {
+      f.push(client);
+    }
+  }
+
+  return f;
+};
+
+WindowGroup.prototype.add = function (client) {
+  if (client.normalWindow) {
+    this.storage[client.windowId] = new Window(client);
+  }
+};
+
+WindowGroup.prototype.remove = function (client) {
+  delete this.storage[client.windowId];
+};
+
+var spacing = {
+  gap: 4, lr: 12, tb: 12
+};
